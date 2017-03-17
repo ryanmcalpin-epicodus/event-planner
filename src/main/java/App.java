0341f7fe1a5fa3392d5c24cbae8316878cc4de92
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-17 13:16:09 -0700

    update ui logic to handle non numbers, negative numbers, and zero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,13 +8,27 @@ public class App {
 
     System.out.println("Welcome to the Event Planner.");
     System.out.println("How many people are attending your event?");
-    attendees = myScanner.nextInt();
 
-    while (attendees <= 0) {
-      System.out.println("That's not enough people! How many people are ACTUALLY attending your event?");
-      attendees = myScanner.nextInt();
+    while (attendees == 0) {
+      try {
+        String attendeesString = myScanner.next();
+        attendees = Integer.parseInt(attendeesString);
+        if (attendees <= 0) {
+          attendees = 0;
+          System.out.println("That's not enough people! How many people are ACTUALLY attending your event?");
+        } else {
+          System.out.println("Great, thanks!");
+        }
+      } catch (NumberFormatException ex) {
+        System.out.println("That's not an number! Try again.");
+      }
     }
 
-    System.out.println("Great.");
+    System.out.println("You're event will have " + attendees + " guests.");
+    // while (attendees <= 0) {
+    //   System.out.println("That's not enough people! How many people are ACTUALLY attending your event?");
+    //   attendees = Integer.parseInt(myScanner.next());
+    // }
+
   }
 }
