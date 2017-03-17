b7e07fbe0ab97bd642b82e081267b6a06612e6dc
Ryan McAlpin <rhmcalpin@gmail.com>
2017-03-17 12:02:51 -0700

    add ui prompts to ask user number of guests and re-ask if the enter 0 or a negative

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("I work!");
+    Scanner myScanner = new Scanner(System.in);
+
+    int attendees = 0;
+
+    System.out.println("Welcome to the Event Planner.");
+    System.out.println("How many people are attending your event?");
+    attendees = myScanner.nextInt();
+
+    while (attendees <= 0) {
+      System.out.println("That's not enough people! How many people are ACTUALLY attending your event?");
+      attendees = myScanner.nextInt();
+    }
+
+    System.out.println("Great.");
   }
 }
